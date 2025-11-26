1e62bf55031ba4c45901409e9822c5c3c55d2095
Ruslan Vudraff <woodruff.l.riley@gmail.com>
2025-11-26 15:20:01 -0800

    Got read/write to work

--- virtual_device.c
+++ virtual_device.c
@@ -12,6 +12,7 @@ includes
 #include <linux/init.h>
 #include <linux/fs.h> //character device operations
 #include <linux/device.h> //to automatically make devices
+#include <linux/uaccess.h> // to add user access to the device
 
 /*==============================================================================
 defines
@@ -29,6 +30,10 @@ prototypes
 static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
 
+//for user access
+static ssize_t device_read(struct file *, char __user *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char __user *, size_t, loff_t *);
+
 
 /*==============================================================================
 global vars
@@ -36,6 +41,7 @@ global vars
 
 //major # is the device # for this driver, minor # is the variant of major #
 static int major_number; 
+static uint32_t device_value = 0;
 static struct class *device_class = NULL;
 static struct device *device_device = NULL;
 
@@ -47,6 +53,8 @@ structs
 static struct file_operations fops = {
     .open = device_open,
     .release = device_release,
+    .read = device_read,
+    .write = device_write,
 };
 
 
@@ -64,6 +72,34 @@ static int device_open(struct inode *inodePath, struct file *filePath) {
 static int device_release(struct inode *inodePath, struct file *filePath) {
     printk(KERN_INFO "virtual_device: Device closed\n");
     return 0;
+}
+
+//called when userspace reads from our device
+static ssize_t device_read(struct file *filePath, char __user *buffer, size_t len, loff_t *offest) {
+    //copy value to user buffer
+    if (copy_to_user(buffer, &device_value, sizeof(device_value))) {
+        return -EFAULT; // error for invalid user space address being sent
+    }
+
+    printk(KERN_INFO "virtual_deivce: Read value: %u\n", device_value);
+    return sizeof(device_value);
+}
+
+//called when userspace writes to our device
+static ssize_t device_write(struct file *filePath, const char __user *buffer, 
+                            size_t len, loff_t *offset) {
+    //write value to user buffer
+    if (len < sizeof(device_value)) {
+        return -EINVAL; // if the size of device value exceeds len, error with invalid argument
+    }
+
+    //copy from userspace to our device
+    if (copy_from_user(&device_value, buffer, sizeof(device_value))) {
+        return -EINVAL; // same as above
+    }
+
+    printk(KERN_INFO "virtual_device: wrote value: %u\n", device_value);
+    return sizeof(device_value);
 }
 
 //driver init
