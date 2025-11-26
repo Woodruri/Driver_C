f04e3ea7fd5d88461175a13a52f6c0f3724ca153
Ruslan Vudraff <woodruff.l.riley@gmail.com>
2025-11-26 13:08:09 -0800

    built and loaded driver using makefile

--- virtual_device.c
+++ virtual_device.c
@@ -11,6 +11,18 @@ Riley Woodruff Nov 26, 2025
 
 
 
-static int __init ()
+static int __init virtual_device_init(void) {
+    printk(KERN_INFO, "virtual_device: Driver loaded\n");
+    return 0;
+}
 
-static void __exit 
+static void __exit virtual_device_exit(void) {
+    printk(KERN_INFO, "virtual_device: Driver unloaded\n");
+}
+
+module_init(virtual_device_init);
+module_exit(virtual_device_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Riley Woodruff");
+MODULE_DESCRIPTION("Learning to write a device driver + use volatile");
